6505688802d96b8549a387cbff8388eda916a876
alex <alex.hmn18@gmail.com>
2025-04-03 12:43:15 +0200

    interface: modification du nom des onglets

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,9 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
     return (
-        <Stack/>
+        <Stack>
+            <Stack.Screen name="index" options={{ title: "Liste des exercices" }} />
+            <Stack.Screen name="timer_param" options={{ title: "Paramètres chrono" }} />
+        </Stack>
     );
 }
